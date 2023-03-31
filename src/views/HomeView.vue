fdc7b74a8e97b8920e8c1d943c1878cf56780b0b
Mikhail Vasilev <mgp-nsk@yandex.ru>
2023-03-31 12:46:47 +0700

    Warning message 31.03.2023

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,12 +15,13 @@
     <div class="container">
       <div class="row my-0 mb-4">
         <div class="col-12">
-          <!--          <div class="alert alert-danger mt-4 mb-0" role="alert">-->
-          <!--            <h4>-->
-          <!--              Обратите внимание, просьба прекратить работу с 17:00 до 18:00.-->
-          <!--              Ведутся технические работы-->
-          <!--            </h4>-->
-          <!--          </div>-->
+          <div class="alert alert-danger mt-4 mb-0" role="alert">
+            <h4>
+              Обратите внимание, просьба прекратить работу 31.03.2023 с 15:00.
+              <br />
+              Ведутся технические работы
+            </h4>
+          </div>
         </div>
         <div class="col-12 col-sm-6 mt-4">
           <CardBootstrapCustomBS46 header class="h-100 shadow">
